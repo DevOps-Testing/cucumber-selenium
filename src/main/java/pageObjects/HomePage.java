14c39dc67bbdb0e343d34803f483d8eb1d6bf2d9
Nicholas Cariss <7630212+ncariss@users.noreply.github.com>
2018-06-26 10:01:35 +0200

    Add file reader and file reader manager

--- src/main/java/pageObjects/HomePage.java
+++ src/main/java/pageObjects/HomePage.java
@@ -3,20 +3,22 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import managers.FileReaderManager;
+
 public class HomePage {
 	WebDriver driver;
-	
-	public HomePage(WebDriver driver) {
+
+	public HomePage(WebDriver driver){
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public void perform_Search(String search) {
-		driver.navigate().to("http://shop.demoqa.com/?s=" + search + "&post_type=product");
+		driver.navigate().to(FileReaderManager.getInstance().getConfigReader().getApplicationUrl() + "/?s=" + search + "&post_type=product");
 	}
-	
+
 	public void navigateTo_HomePage() {
-		driver.get("http://www.shop.demoqa.com");
+		driver.get(FileReaderManager.getInstance().getConfigReader().getApplicationUrl());
 	}
 
 }
--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,10 +1,13 @@
 package stepDefinitions;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import managers.FileReaderManager;
 import managers.PageObjectManager;
 import pageObjects.CartPage;
 import pageObjects.CheckoutPage;
@@ -17,14 +20,14 @@ public class Steps {
 	ProductListingPage productListingPage;
 	CartPage cartPage;
 	CheckoutPage checkoutPage;
-	PageObjectManager pageObjectManager;
-	
+	PageObjectManager pageObjectManager;	
+
 	@Given("^user is on Home Page$")
 	public void user_is_on_Home_Page(){
-		System.setProperty("webdriver.chrome.driver","C:\\tools\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", FileReaderManager.getInstance().getConfigReader().getDriverPath());
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(FileReaderManager.getInstance().getConfigReader().getImplicitlyWait(), TimeUnit.SECONDS);
 		pageObjectManager = new PageObjectManager(driver);
 		homePage = pageObjectManager.getHomePage();
 		homePage.navigateTo_HomePage();	
@@ -48,28 +51,27 @@ public class Steps {
 		cartPage.clickOn_Cart();
 		cartPage.clickOn_ContinueToCheckout();	
 	}
-	
+
 	@When("^enter personal details on checkout page$")
-	public void enter_personal_details_on_checkout_page() throws InterruptedException {
+	public void enter_personal_details_on_checkout_page(){
 		checkoutPage = pageObjectManager.getCheckoutPage();
 		checkoutPage.fill_PersonalDetails();	
 	}
-	
+
 	@When("^select same delivery address$")
-	public void select_same_delivery_address() throws InterruptedException{
+	public void select_same_delivery_address(){
 		checkoutPage.check_ShipToDifferentAddress(false);
 	}
-	
+
 	@When("^select payment method as \"([^\"]*)\" payment$")
 	public void select_payment_method_as_payment(String arg1){
 		checkoutPage.select_PaymentMethod("CheckPayment");
 	}
 
 	@When("^place the order$")
-	public void place_the_order() throws InterruptedException {
+	public void place_the_order() {
 		checkoutPage.check_TermsAndCondition(true);
-		checkoutPage.clickOn_PlaceOrder();
-		
+		checkoutPage.clickOn_PlaceOrder();		
 		driver.quit();
 	}	
 }
